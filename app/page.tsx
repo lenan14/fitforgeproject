91b115fd365de27df036d53d28ffda55f7301577
Raphael Dean Mendones <RAPHAELDEAN.MENDONES07@myhunter.cuny.edu>
2026-03-13 15:38:10 -0400

    Homepage Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,97 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // [Auto-Login] \\
+  useEffect(() => {
+    const savedUser = localStorage.getItem("username");
+
+    if (savedUser) {
+      router.push("/feed");
+    }
+  }, [router]);
+
+  // [Login] \\
+  const handleLogin = () => {
+    const users = JSON.parse(localStorage.getItem("users") || "{}");
+
+    if (!users[username]) {
+      alert("Account doesn't exists")
+      return;
+    }
+
+    if (users[username] !== password) {
+      alert("Incorrect password.");
+      return;
+    }
+
+    localStorage.setItem("username", username);
+
+    router.push("/feed")
+  };
+
+  // [Create Account] \\
+  const handleCreate = () => {
+    const users = JSON.parse(localStorage.getItem("users") || "{}");
+
+    if (users[username]) {
+      alert("Username already exists.");
+      return;
+    }
+
+    users[username] = password;
+
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("username", username);
+
+    router.push("/feed");
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="Intro-Container">
+
+      {/* [Introduction] */}
+      <h1 className="Intro-Title">Welcome to Fit Forge</h1>
+
+      <p className="Intro-Description">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum."
+      </p>
+
+      {/* Autehntication */}
+      <div className="Authen-Container">
+
+        <h2 className="Authen-Title">Welcome to the Playground</h2>
+
+        <p className="Authen-Description">
+          Login or Create an Account.
+        </p>
+
+        <br /><br />
+
+        <input
+          type="password" 
+          className="Authen-Input"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <br /><br />
+
+        <button className="Authen-Btn" onClick={handleCreate}>Regiter</button>
+
+      </div>
     </div>
-  );
+  )
 }
